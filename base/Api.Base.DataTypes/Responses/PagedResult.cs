cef260cd5f17436d775c0a1bf3d72312e37ffe5d
Panagiotis Katsampiris <p.katsampiris@codehub.gr>
2020-08-19 11:54:33 +0300

    Added PagedResult classes to hold paged data.

---
+++ base/Api.Base.DataTypes/Responses/PagedResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Api.Base.DataTypes.Responses
+{
+    [DataContract]
+    public class PagedResult<T> : PagedResultBase
+    {
+        [DataMember]
+        public List<T> Items { get; set; } = new List<T>();
+    }
+}
---
+++ base/Api.Base.DataTypes/Responses/PagedResultBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace Api.Base.DataTypes.Responses
+{
+    [DataContract]
+    public abstract class PagedResultBase
+    {
+        [DataMember]
+        public int CurrentPage { get; set; }
+        [DataMember]
+        public int CurrentPageSize { get; set; }
+        [DataMember]
+        public int TotalPages { get; set; }
+        [DataMember]
+        public int TotalEntries { get; set; }
+    }
+}
