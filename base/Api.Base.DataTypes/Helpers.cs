8debf52c6ed4e5703c73d53bf33f9d8e77d9d450
Panagiotis Katsampiris <p.katsampiris@codehub.gr>
2020-08-19 12:05:32 +0300

    Add PageSettings to get from config. Add Helpers to get valid current page and pageSize.

---
+++ base/Api.Base.DataTypes/Helpers.cs
@@ -0,0 +1,36 @@
+﻿namespace Api.Base.DataTypes
+{
+    public static class Helpers
+    {
+        public static int GetPageOrDefault(this PageSettings pageSettings, int? currentPage)
+        {
+            if (currentPage.HasValue && currentPage.Value > 0)
+            {
+                return currentPage.Value;
+            }
+
+            return pageSettings != null
+                ? pageSettings.DefaultPage
+                : 1;
+        }
+
+        public static int GetItemsOrDefault(this PageSettings pageSettings, int? howMany)
+        {
+            if (howMany.HasValue)
+            {
+                if (pageSettings != null && pageSettings.MaxPageSize > 0 && howMany.Value > pageSettings.MaxPageSize)
+                {
+                    return pageSettings.MaxPageSize;
+                }
+                if (howMany > 0)
+                {
+                    return howMany.Value;
+                }
+            }
+
+            return pageSettings != null
+                ? pageSettings.DefaultPageSize
+                : 100;
+        }
+    }
+}
